7e51f5e934669d0ae7778a5db5060c7824835968
aish-8 <amaishbi8@gmail.com>
2026-02-27 11:13:09 +0530

    api files

--- urls.py
+++ urls.py
@@ -1,24 +1,11 @@
-"""notesapp URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
+from django.urls import path
+from .views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('', getRoutes, name="routes"),
+    path('notes/', getNotes, name="notes"),
+    path('notes/<str:pk>/update/', updateNote, name="update-note"),
+    path('notes/<str:pk>/delete/', deleteNote, name="delete-note"),
+    path('notes/create/', createNote, name="create-note"),
+    path('notes/<str:pk>/', getNote, name="note"),
 ]
